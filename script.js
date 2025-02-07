d987b0baeb8e7c68a5c0f5d208a413d07e67d96d
Md. Redowanul Haq Rafi <142563065+RHRafi2003@users.noreply.github.com>
2025-02-07 14:03:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Website Loaded!");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("%cWebsite Loaded Successfully!", "color: #0073e6; font-weight: bold;");
 });
