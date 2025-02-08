9bd5917a3d2f622a4f491d314c7caba965d6ac58
Md. Redowanul Haq Rafi <redowanulhaqrafi@gmail.com>
2025-02-08 19:38:12 +0600

    Update

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
-                observer.unobserve(entry.target); // Improve performance by unobserving
+                observer.unobserve(entry.target); 
             }
         });
     }, { threshold: 0.1 });
